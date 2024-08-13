451abef995bb24b037471d6db9a2dac9fb448647
Biggee Lucloner <lucloner@hotmail.com>
2024-08-13 15:44:31 +0800

    add check test url 2

--- src/main/java/net/vicp/biggee/aot/vpn/expressvpn/Dialer/DialerApplication.java
+++ src/main/java/net/vicp/biggee/aot/vpn/expressvpn/Dialer/DialerApplication.java
@@ -1,11 +1,14 @@
 package net.vicp.biggee.aot.vpn.expressvpn.Dialer;
 
+import net.vicp.biggee.aot.vpn.expressvpn.Dialer.util.RunShell;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableConfigurationProperties(RunShell.class)
 public class DialerApplication {
 
 	public static void main(String[] args) {
